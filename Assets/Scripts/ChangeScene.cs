2cba833b93949e5bc0916d5e70f45bba91788a29
David Svensson <david@Admins-MacBook-Pro-2.local>
2018-09-17 12:40:51 +0200

    First commit

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    AsyncOperation async;
+
+    private void Start()
+    {
+        Player.Instance.transform.position = Vector3.zero;
+
+        if (SceneManager.GetActiveScene().name == "Scene1")
+        {
+            async = SceneManager.LoadSceneAsync("Scene2");
+            async.allowSceneActivation = false;
+        }
+        else if (SceneManager.GetActiveScene().name == "Scene2")
+        {
+            async = SceneManager.LoadSceneAsync("Scene1");
+            async.allowSceneActivation = false;
+        }
+
+    }
+
+
+    private void OnMouseDown()
+    {
+        async.allowSceneActivation = true;
+       Debug.Log("points: " + Player.Instance.points);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private static Player _instance;
+
+
+    public static Player Instance { get { 
+
+            if (_instance == null)
+            {
+                _instance = Instantiate(Resources.Load<GameObject>("Player")).GetComponent<Player>();
+            }
+
+            return _instance; } 
+    }
+
+
+    public int points = 0;
+
+    private void Awake()
+    {
+      
+       if (_instance != null && _instance != this)
+            Destroy(gameObject);
+
+        else {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        points++;
+    }
+}
